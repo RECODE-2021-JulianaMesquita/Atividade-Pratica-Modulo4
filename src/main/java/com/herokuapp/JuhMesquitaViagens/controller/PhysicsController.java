4634fb90e861ec24b8494c2a6d11eeb38c524018
Juliana Mesquita <julianamariasousamesquita@id.uff.br>
2022-02-02 20:32:31 -0300

    Adicionando controller pessoa física, cliente  - PhysicsController

---
+++ src/main/java/com/herokuapp/JuhMesquitaViagens/controller/PhysicsController.java
@@ -0,0 +1,61 @@
+package com.herokuapp.JuhMesquitaViagens.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.herokuapp.JuhMesquitaViagens.exception.ResourceNotFoundException;
+import com.herokuapp.JuhMesquitaViagens.model.Physics;
+import com.herokuapp.JuhMesquitaViagens.repository.PhysicsRepository;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/api/v1/")
+public class PhysicsController {
+    @Autowired
+    private PhysicsRepository physicsRepository;
+
+    // get all pessoas físicas cadastrados
+    @GetMapping("/physics")
+    public List<Physics> getAllPhysics() {
+        return physicsRepository.findAll();
+    }
+
+    // create pessoa física rest api
+    @PostMapping("/physics")
+    public Physics createLogin(@RequestBody Physics createPhysics) {
+        return physicsRepository.save(createPhysics);
+    }
+
+    // get pessoa física by id rest api
+    @GetMapping("/physics/{id}")
+    public ResponseEntity <Physics> getLoginById(@PathVariable Long id) {
+        Physics physicsId = physicsRepository.findById(id).orElseThrow(() - > new ResourceNotFoundException("Não existe no banco de dados pessoa física com o id = "  + id));
+        return ResponseEntity.ok(physicsId);
+    }
+
+    // update usuario rest api
+    @PutMapping("/physics/{id}")
+     public ResponseEntity <Physics> updateLogin(@PathVariable Long id, @RequestBody Physics physicsDetails) {
+        Physics physicsEdit = physicsRepository.findById(id).orElseThrow(() - > new ResourceNotFoundException("Não existe no banco de dados usuario com o id = "  + id));
+ 
+        physicsEdit.setCpf(physicsDetails.getCpf());
+        physicsEdit.setPackages(physicsDetails.getPackages());
+
+        Physics updatedPhysics = physicsRepository.save(physicsEdit);
+        return ResponseEntity.ok(updatedPhysics);
+    }
+
+}
